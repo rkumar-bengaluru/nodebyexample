5e26f75df68bd742d15c76b701865b361423213b
rkumar-bengaluru <rupak.kumar.ambasta@gmail.com>
2022-09-11 18:19:23 +0530

    z

--- server/app.js
+++ server/app.js
@@ -8,8 +8,8 @@ app.use(express.static(htmlPath));
 
 var server = app.listen(3000, function () {
     var host = 'localhost';
-    var server = app.listen(process.env.PORT || 8080, () => {
-        console.log('Server is started on 127.0.0.1:'+ (process.env.PORT || 8080))
+    var server = app.listen(process.env.PORT || 8081, () => {
+        console.log('Server is started on 127.0.0.1:'+ (process.env.PORT || 8081))
     })
 });
 
